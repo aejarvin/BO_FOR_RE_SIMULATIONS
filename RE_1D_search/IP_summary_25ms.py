a8ded0a383e26e454faf58cdef0ec72eb553a954
Aaro Järvinen <87468858+aejarvin@users.noreply.github.com>
2022-06-17 12:48:57 +0300

    Update IP_summary_25ms.py

--- RE_1D_search/IP_summary_25ms.py
+++ RE_1D_search/IP_summary_25ms.py
@@ -3,6 +3,14 @@ import numpy as np
 
 # Define IP summary function
 def IP_summary_25ms(data):
+    ''' This function simply maps the plasma current to a standardized 
+    timebase between 0 and 25 ms.
+    
+    Input:
+        data : np.array of 2 columns: time, Ip
+    Return:
+        IPout : np.array of 1 column: Ip mapped to a standardized timebase
+    '''
     timev = data[0,:]
     IPv = data[1,:]
     timebase = 1e-3*25.0*np.array(range(0,2000))/2000.0
